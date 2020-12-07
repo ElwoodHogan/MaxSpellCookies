5f321befa32f32baa5e24197fd55f55ecebbf7f8
ElwoodHogan <73083286+ElwoodHogan@users.noreply.github.com>
2020-12-07 15:07:03 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,8 @@
+Game.registerMod('leon',{
 				init:function(){
 					Game.registerHook('draw',function(){Game.mods['leon'].addIntro();});
 				},
+				
 				addIntro:function(text){
 					var val=(Game.cookiesPs*60*30)/.15;
 					var added = 'dab';
@@ -31,4 +33,5 @@
 					Game.playerIntro=output;
 					if (!l('bakerySubtitle')) l('bakeryName').insertAdjacentHTML('afterend','<div id="bakerySubtitle" class="title" style="text-align:center;position:absolute;left:0px;right:0px;bottom:-90px;font-size:15px;pointer-events:none;text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;opacity:1;"></div>');
 					l('bakerySubtitle').textContent=Game.playerIntro+' '+added;
-				}
+				},
+			});
