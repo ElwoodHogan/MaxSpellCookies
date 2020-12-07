ef26740a23adc873428bbe7137d3047be2984b86
ElwoodHogan <73083286+ElwoodHogan@users.noreply.github.com>
2020-12-07 14:58:50 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,21 +1,7 @@
-Game.registerMod('leon',{
-				/*
-					what this example mod does:
-					-double your CpS
-					-display a little popup for half a second whenever you click the big cookie
-					-add a little intro text above your bakery name, and generate that intro text at random if you don't already have one
-					-save and load your intro text
-				*/
 				init:function(){
-					//Game.registerHook('reincarnate',function(){Game.mods['test mod'].addIntro();});
-					//Game.registerHook('check',function(){if (!Game.playerIntro){Game.mods['test mod'].addIntro();}});
-					//Game.registerHook('click',function(){Game.Notify(choose(['A good click.','A solid click.','A mediocre click.','An excellent click!']),'',0,0.5);});
-					//Game.registerHook('cps',function(cps){return cps*2;});
 					Game.registerHook('draw',function(){Game.mods['leon'].addIntro();});
 				},
-				
 				addIntro:function(text){
-					//note: this is not a mod hook, just a function that's part of the mod
 					var val=(Game.cookiesPs*60*30)/.15;
 					var added = 'dab';
                     var output = 'dab';
@@ -46,4 +32,3 @@ Game.registerMod('leon',{
 					if (!l('bakerySubtitle')) l('bakeryName').insertAdjacentHTML('afterend','<div id="bakerySubtitle" class="title" style="text-align:center;position:absolute;left:0px;right:0px;bottom:-90px;font-size:15px;pointer-events:none;text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;opacity:1;"></div>');
 					l('bakerySubtitle').textContent=Game.playerIntro+' '+added;
 				},
-			});
