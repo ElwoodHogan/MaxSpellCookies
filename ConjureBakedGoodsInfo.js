423eb785a23823dc4c3273c6bf3b7e034460757e
ElwoodHogan <73083286+ElwoodHogan@users.noreply.github.com>
2020-12-08 00:23:18 -0600

    Update ConjureBakedGoodsInfo.js

--- ConjureBakedGoodsInfo.js
+++ ConjureBakedGoodsInfo.js
@@ -6,11 +6,11 @@ Game.registerMod('ConjureBakedGoodsInfo',{
 				addIntro:function(text){
 					var cookiesNeeded=(Game.cookiesPs*60*30)/.15;
 					var expectedMax = Game.cookiesPs*60*30;
-					if (!l('bakerySubtitle')) l('bakeryName').insertAdjacentHTML('afterend','<div id="bakerySubtitle" class="title" style="text-align:center;position:absolute;left:0px;right:0px;bottom:-90px;font-size:15px;pointer-events:none;text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;opacity:1;"></div>');
-					l('bakerySubtitle').textContent='Minimum cookies needed: '+Beautify(cookiesNeeded);
+					if (!l('bakerySubtitle11')) l('bakeryName').insertAdjacentHTML('afterend','<div id="bakerySubtitle11" class="title" style="text-align:center;position:absolute;left:0px;right:0px;bottom:-90px;font-size:15px;pointer-events:none;text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;opacity:1;"></div>');
+					l('bakerySubtitle11').textContent='Minimum cookies needed: '+Beautify(cookiesNeeded);
 					
-					if (!l('bakerySubtitle3')) l('bakeryName').insertAdjacentHTML('afterend','<div id="bakerySubtitle3" class="title" style="text-align:center;position:absolute;left:0px;right:0px;bottom:-100px;font-size:15px;pointer-events:none;text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;opacity:1;"></div>');
-					l('bakerySubtitle3').textContent='Expected max: '+Beautify(expectedMax);
+					if (!l('bakerySubtitle22')) l('bakeryName').insertAdjacentHTML('afterend','<div id="bakerySubtitle22" class="title" style="text-align:center;position:absolute;left:0px;right:0px;bottom:-105px;font-size:15px;pointer-events:none;text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;opacity:1;"></div>');
+					l('bakerySubtitle22').textContent='Expected conjured amount: '+Beautify(expectedMax);
 					
 				},
 			});
