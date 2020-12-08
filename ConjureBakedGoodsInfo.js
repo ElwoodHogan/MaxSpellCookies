a11ed6407aadb467ac1fbd1e69f7c08134aa9ee6
ElwoodHogan <73083286+ElwoodHogan@users.noreply.github.com>
2020-12-08 00:21:38 -0600

    Update ConjureBakedGoodsInfo.js

--- ConjureBakedGoodsInfo.js
+++ ConjureBakedGoodsInfo.js
@@ -7,10 +7,10 @@ Game.registerMod('ConjureBakedGoodsInfo',{
 					var cookiesNeeded=(Game.cookiesPs*60*30)/.15;
 					var expectedMax = Game.cookiesPs*60*30;
 					if (!l('bakerySubtitle')) l('bakeryName').insertAdjacentHTML('afterend','<div id="bakerySubtitle" class="title" style="text-align:center;position:absolute;left:0px;right:0px;bottom:-90px;font-size:15px;pointer-events:none;text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;opacity:1;"></div>');
-					l('bakerySubtitle').textContent=Beautify(cookiesNeeded);
+					l('bakerySubtitle').textContent='Minimum cookies needed: '+Beautify(cookiesNeeded);
 					
-					if (!l('bakerySubtitle2')) l('bakeryName').insertAdjacentHTML('afterend','<div id="bakerySubtitle2" class="title" style="text-align:center;position:absolute;left:0px;right:0px;bottom:-100px;font-size:15px;pointer-events:none;text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;opacity:1;"></div>');
-					l('bakerySubtitle2').textContent=Beautify(expectedMax);
+					if (!l('bakerySubtitle3')) l('bakeryName').insertAdjacentHTML('afterend','<div id="bakerySubtitle3" class="title" style="text-align:center;position:absolute;left:0px;right:0px;bottom:-100px;font-size:15px;pointer-events:none;text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;opacity:1;"></div>');
+					l('bakerySubtitle3').textContent='Expected max: '+Beautify(expectedMax);
 					
 				},
 			});
